e5533f52452f2f628f9e8a20d0f4ad78ae5cf50f
BochkarevAS <snake-vl@mail.ru>
2019-03-17 21:51:27 +1000

    Add Search for Elastica

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\CatalogDto;
+use App\Entity\Catalog;
 use App\Service\Context;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\ElasticaBundle\Doctrine\RepositoryManager;
 
 class HomeController extends AbstractController
 {
     private $context;
 
-    public function __construct(Context $context)
+    /**
+     * @var RepositoryManager
+     */
+    private $manager;
+
+    public function __construct(Context $context, RepositoryManager $manager)
     {
         $this->context = $context;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("/search", name="search", methods="GET|POST", options={"expose"=true})
+     */
+    public function search(Request $request)
+    {
+        $data = new CatalogDto();
+
+        $result = $this->manager->getRepository(Catalog::class)->search($data);
     }
 
     /**
--- src/Repository/CatalogElasticaRepository.php
+++ src/Repository/CatalogElasticaRepository.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use App\DTO\ProductDto;
+use App\DTO\CatalogDto;
 
 class CatalogElasticaRepository extends EntityRepository
 {
@@ -15,23 +15,23 @@ class CatalogElasticaRepository extends EntityRepository
             ->orderBy('c.id', 'ASC');
     }
 
-    public function search(ProductDto $product)
+    public function search(CatalogDto $catalog)
     {
         $qb   = new \Elastica\QueryBuilder();
         $bool = $qb->query()->bool();
 
-        if (!empty($product->name)) {
-            $bool->addMust($qb->query()->bool()->addMust($qb->query()->match('name', $product->name)));
+        if (!empty($catalog->name)) {
+            $bool->addMust($qb->query()->bool()->addMust($qb->query()->match('name', $catalog->name)));
         }
 
-        if (null !== $product->brand) {
-            $bool->addMust($qb->query()->bool()->addMust($qb->query()->term(['brand.id' => $product->brand->getId()])));
+        if (null !== $catalog->brand) {
+            $bool->addMust($qb->query()->bool()->addMust($qb->query()->term(['brand.id' => $catalog->brand->getId()])));
         }
 
-        if (!empty($product->marks)) {
+        if (!empty($catalog->marks)) {
 
             $bool->addMust($qb->query()->bool()->addMust(
-                $qb->query()->nested()->setPath('marks')->setQuery($qb->query()->bool()->addMust($qb->query()->term(['marks.id' => $product->marks->getId()])))
+                $qb->query()->nested()->setPath('marks')->setQuery($qb->query()->bool()->addMust($qb->query()->term(['marks.id' => $catalog->marks->getId()])))
             ));
         }
     }
